cad674b5fd6bfcc7c93129a355cb4d685407dcb3
serigala16 <150578019+serigala16@users.noreply.github.com>
2023-11-12 16:15:20 +0700

    Update add-tr.sh

--- xray/add-tr.sh
+++ xray/add-tr.sh
@@ -39,7 +39,7 @@ read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 read -p "Limit ip (DEVICE) : " Limit
 read -p "Limit bw (GB) : " bw
-read -p "Limit quota (GB) : quota
+read -p "Limit quota (GB) : " quota
 sed -i '/#trojanws$/a\#! '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#trojangrpc$/a\#! '"$user $exp"'\
@@ -63,10 +63,10 @@ echo -e "Link WS      : ${trojanlink}"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "Link GRPC    : ${trojanlink1}"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Limit ip    : $Limit (DEVIC)"
-echo -e "Limit bw : $bw (GB)"
-echo -e "Limit quota : $quota (GB)"
-echo -e "Expired On     : $exp (HARI)" | tee -a /etc/log-create-user.log
+echo -e "Limit ip     : $Limit (DEVIC)"
+echo -e "Limit bw     : $bw (GB)"
+echo -e "Limit quota  : $quota (GB)"
+echo -e "Expired On   : $exp (HARI)" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
